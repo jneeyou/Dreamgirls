cacb49e8318e5fcf58781e1c0e6c525b50f82ac4
jneeyou <1275650007@qq.com>
2016-03-23 00:02:47 +0800

    update entity all

--- Classes/Player.h
+++ Classes/Player.h
@@ -3,14 +3,50 @@
 #define __PLAYER_H__
 
 #include "BaseEntity.h"
-
+#include "GlobalDefine.h"
 
 /************************************************* 玩家类 **********************************************/
 
 class Player : public BaseEntity
 {
 public:
-	virtual bool init();
+	Player();
+	~Player();
+
+	/* 通过csv配置文件创建 */
+	CREATE_FUNC_0(Player, const char);
+
+	virtual bool init(const char* csvFilePath);
+
+protected:
+	/* 跑 */
+	void onRun();
+
+	/* 走 */
+	void onWalk();
+
+	/* 跳 */
+	void onJump();
+
+	/* 抓 */
+	void onCatch ();
+
+	/* 攻击 */
+	void onAttack();
+
+	/* 增加生命 */
+	void onIncreateLife(int lifeValue);
+
+	/* 获得道具 */
+	void onGetTool();
+
+	/* 使用道具 */
+	void onUseTool();
+
+protected:
+	CC_SYNTHESIZE(int, m_curAct, CurAct);						// 当前攻击力
+	CC_SYNTHESIZE(int, m_curWalkDistance, CurWalkDistance);		// 已走距离
+
 };
 
 #endif // !__PLAYER_H__
